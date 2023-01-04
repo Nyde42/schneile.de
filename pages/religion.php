90b7fe865cb7e0c04547da4f59242b13246cd1b4
Kanashimi <nydemc@gmail.com>
2023-01-04 08:53:25 +0100

    Improve project structure

--- pages/religion.php
+++ pages/religion.php
@@ -1,13 +1,13 @@
 <h1>Der Schneilismus</h1>
 
 <p>
-	Willkommen zu unserer Religion, die sich um unseren Gott <u>Schneile</u> bildet!
+	Willkommen zu unserer Religion, die sich um unseren Gott <span class="holy">Schneile</span> bildet!
 </p><p>
 	Auf dieser Website legen wir die Grundsätze unseres Glaubens dar und hoffen, dass wir dich damit überzeugen können, ebenfalls ein Sigma zu werden und zum Schneilismus zu konvertieren.
 </p>
 
 
-<h2>Das <u>Sigma Male Grindset</u></h2>
+<h2>Das <strong>Sigma Male Grindset</strong></h2>
 
 <p>
 	Dies sind keine verpflichtenden Regeln, sondern Richtlinien, die einem ein Dasein als wahrer Sigma ermöglichen sollen.
@@ -25,10 +25,10 @@
 		"Schmei&szlig;e den Lehrer raus, wenn er zu viel redet"      => "Selbstständigkeit",
 		"Schaue nicht auf die Uhr, bestimme die Zeit selbst"         => "Zeitmanagement",
 		"Gib deinem Chef Urlaub"                                     => "Entscheidungskraft"
-	] as $k => $v) echo str_pad("\n\t<tr><td>&nbsp;&nbsp;&#x2022;&nbsp;&nbsp;$k</td>", 100) . "<td>&nbsp;&nbsp;&rarr;&nbsp;&nbsp;<b>$v</b></td></tr>";
+	] as $k => $v) echo str_pad("\n\t<tr><td>&nbsp;&nbsp;&#x2022;&nbsp;&nbsp;$k</td>", 105) . "<td>&nbsp;&nbsp;&rarr;&nbsp;&nbsp;<b>$v</b></td></tr>";
 ?>
 </table>
-<p>So wurde es uns einst vom einzig wahren <u>Schneile</u> gepredigt.</p>
+<p>So wurde es uns einst vom einzig wahren <span class="holy">Schneile</span> gepredigt.</p>
 <iframe
 	width      ="480"
 	height     ="270"
@@ -44,8 +44,8 @@
 <h2>Ausruf</h2>
 
 <table border=0>
-	<tr><td>Drahe:</td>      <td>&nbsp;&nbsp;&nbsp;&nbsp;<audio controls src="files/drahe.mp3"      ><a href="files/drahe.mp3">Download</a></audio></td></tr>
-	<tr><td>Drahe Alder:</td><td>&nbsp;&nbsp;&nbsp;&nbsp;<audio controls src="files/drahe_alder.mp3"><a href="files/drahe.mp3">Download</a></audio></td></tr>
+	<tr><td>Drahe:</td>      <td>&nbsp;&nbsp;&nbsp;&nbsp;<audio controls src="/files/drahe.mp3"      ><a href="/files/drahe.mp3">Download</a></audio></td></tr>
+	<tr><td>Drahe Alder:</td><td>&nbsp;&nbsp;&nbsp;&nbsp;<audio controls src="/files/drahe_alder.mp3"><a href="/files/drahe.mp3">Download</a></audio></td></tr>
 </table>
 
 
@@ -53,25 +53,25 @@
 
 <h3>Götter und Ideale</h3>
 <ul>
-	<li><u>Gigachad</u> ist das Sigma-Male-Ideal, sowohl körperlich als auch geistig, nach dem jeder von uns streben soll.</li>
-	<li><u>Schneile</u> ist die ultimative Verkörperung des <u>Gigachad</u>-Ideals und soll infolgedessen unser Gott sein.</li>
+	<li><span class="holy">Gigachad</span> ist das Sigma-Male-Ideal, sowohl körperlich als auch geistig, nach dem jeder von uns streben soll.</li>
+	<li><span class="holy">Schneile</span> ist die ultimative Verkörperung des <span class="holy">Gigachad</span>-Ideals und soll infolgedessen unser Gott sein.</li>
 </ul>
 
 <h3>Propheten</h3>
 <p>
-	Diese Personen stehen in engem Kontakt zu unserem Gott <u>Schneile</u> und dienen unter anderem als Vermittler zwischen ihm und den Sterblichen.
-	Was sie sagen, gilt als Gesetz, übertrumpft nur von den Weisheiten des einzig wahren <u>Schneile</u> selbst.
-<p/>
-<b><ul>
-	<li>Kanashimi</li>
-	<li>Darkman</li>
-	<li>ConShuffle</li>
-	<li>Ikan Nimer</li>
-</ul></b>
+	Diese Personen stehen in engem Kontakt zu unserem Gott <span class="holy">Schneile</span> und dienen unter anderem als Vermittler zwischen ihm und den Sterblichen.
+	Was sie sagen, gilt als Gesetz, übertrumpft nur von den Weisheiten des einzig wahren <span class="holy">Schneile</span> selbst.
+</p>
+<ul>
+	<li><b>Kanashimi</b></li>
+	<li><b>Darkman</b></li>
+	<li><b>ConShuffle</b></li>
+	<li><b>Ikan Nimer</b></li>
+</ul>
 
 <h3>Mitglieder</h3>
 <p>
-	Diese Personen gehören alle dem Schneilismus an und helfen, das <u>Gigachad</u>-Mindset auf der Welt zu verbreiten.
+	Diese Personen gehören alle dem Schneilismus an und helfen, das <span class="holy">Gigachad</span>-Mindset auf der Welt zu verbreiten.
 	Personen, die der Gründung unserer Glaubensrichtung beiwohnten, sind hervorgehoben.
 </p>
 <ul>
@@ -84,7 +84,7 @@
 
 <h3>Ausgestoßene</h3>
 <p>
-	Der einzige Ausgestoßene ist bisher der böse, verräterische <s>Seelenbrenner</s>.<br>
-	Einst ein Prophet und scheinbarer Freund, kehrte er plötzlich eines Tages seinen schneilistischen Kameraden den Rücken, um dem konkurrierenden Gott <s>Baui</s> zu huldigen...
-	<s>Seelenbrenner</s> trägt seither auch den Spitznamen "Judas".
+	Der einzige Ausgestoßene ist bisher der böse, verräterische <span class="evil">Seelenbrenner</span>.<br>
+	Einst ein Prophet und scheinbarer Freund, kehrte er plötzlich eines Tages seinen schneilistischen Kameraden den Rücken, um dem konkurrierenden Gott <span class="evil">Baui</span> zu huldigen...
+	<span class="evil">Seelenbrenner</span> trägt seither auch den Spitznamen "Judas".
 </p>
